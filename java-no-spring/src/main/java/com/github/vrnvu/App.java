dd6b8d1324ab39f0c171e8632f57a20c1b67ea39
ad <hi@arnaudiaz.com>
2025-01-15 12:45:03 +0100

    creates hello world context

--- java-no-spring/src/main/java/com/github/vrnvu/App.java
+++ java-no-spring/src/main/java/com/github/vrnvu/App.java
@@ -1,10 +1,27 @@
 package com.github.vrnvu;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.sun.net.httpserver.HttpServer;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        var port = 8080;
+        var server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", (exchange) -> {
+            exchange.sendResponseHeaders(200, 0);
+            exchange.getResponseBody().write("Hello World!".getBytes());
+            exchange.getResponseBody().close();
+        });
+        logger.log(Level.INFO, "Server started on port {0}", port);
+        server.start();
     }
 }
